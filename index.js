2a75bf748c3532258df92e8718b773c0ed35dee3
neel-1905 <neelnarwadkar@gmail.com>
2023-01-10 12:54:07 +0530

    new vercel config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const userRoute = require("./routes/users");
 const courseRoute = require("./routes/courses");
 const cors = require("cors");
